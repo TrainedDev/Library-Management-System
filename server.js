34c656095d2d291ccf92de53cd8644a37eaeaf9c
Assassinyogeshh <legendaryassassins388@gmail.com>
2025-03-21 16:56:14 +0530

    first change

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ const app = express();
 
 app.use(express.json());
 
+// authorData 
 const authorsData = [
     { name: 'J.K. Rowling', birthdate: '1965-07-31', email: 'jkrowling@books.com' },
     { name: 'George R.R. Martin', birthdate: '1948-09-20', email: 'grrmartin@books.com' }
 ];
 
+// genreData
 const genresData = [
     { name: 'Fantasy', description: 'Magical and mythical stories.' },
     { name: 'Drama', description: 'Fiction with realistic characters and events.' }
 ];
 
+// booksData
 const booksData = [
     { title: 'Harry Potter and the Philosopher\'s Stone', description: 'A young wizard\'s journey begins.', publicationYear: 1997, authorId: 1 },
     { title: 'Game of Thrones', description: 'A medieval fantasy saga.', publicationYear: 1996, authorId: 2 }
